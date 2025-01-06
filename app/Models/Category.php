7a1829018fe3e0ecedf22bb33a80a3abbd5e3d6d
Mya Htut Khaung <myahtutkhaung2002@gmail.com>
2025-01-06 09:28:23 +0630

    Fix Category @06-01-2025

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,35 +9,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Category extends Model
 {
     use HasFactory;
+    protected $fillable = ['name'];
 
-    public function index(Request $request)
-    {
-        $categories = Category::withCount('products')->orderBy('name', 'ASC')->paginate(10);
-        return view('categories.index', compact('categories'));
-    }
-
-    public function create()
-    {
-        return view('categories.create');
-    }
-
-    public function store(Request $request)
-    {
-        $validatedData = $request->validated();
-
-        if(Category::create($validatedData)) {
-            return redirect()->route('categories.index')->with('status', __('Category :category added', ['category' => $validatedData['name']]));
-        }
-        return back()->with('error', 'Category creation failed');
-    }
-
-    public function show()
-    {
-        //
-    }
-
-    public function edit(Category $category)
-    {
-        return view('categories.edit', ['category' => $category]);
-    }
+    
 }
